071852dbf9894133fe82d560ed5c60d1f741377d
Chris <66511731+aioooi@users.noreply.github.com>
2019-02-05 19:11:06 +0100

    implement RNGBenchmark

---
+++ src/rngbenchmark.h
@@ -0,0 +1,42 @@
+#ifndef RNGBENCHMARK_H
+#define RNGBENCHMARK_H
+
+#include <iostream>
+#include <string>
+
+#include "benchmark.h"
+
+
+namespace Benchmark {
+
+template<class RNG, class DIST, class FLOATTYPE>
+class RNGBenchmark : public Benchmark
+{
+public:
+    RNGBenchmark(const std::string &name, const DIST &distribution,
+                 const unsigned int seed, const unsigned long trials)
+        : Benchmark(name), distribution(distribution), seed(seed), trials(trials) {}
+
+protected:
+    void benchmark() {
+        RNG rng(seed);
+        for (unsigned long i = 0; i < trials; ++i) {
+            FLOATTYPE sample = distribution(rng);
+        }
+    }
+
+    void post_benchmark() {
+        std::cout << "Benchmark " << name << ":" << std::endl
+                  << trials << " in " << duration.count() << " seconds, i.e., "
+                  << trials / duration.count() << " samples per second"
+                  << std::endl;
+    }
+
+    DIST distribution;
+    unsigned int seed;
+    unsigned long trials;
+};
+
+} // namespace benchmark
+
+#endif // RNGBENCHMARK_H
