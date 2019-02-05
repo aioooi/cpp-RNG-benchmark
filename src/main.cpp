cb1eec44c9a500d1edc2faaae839897fef18af8b
Chris <66511731+aioooi@users.noreply.github.com>
2019-02-05 19:12:42 +0100

    main() runs benchmarks for various RNGs and normal distributions

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,101 @@
-#include <iostream>
+#include <random>
 
-using namespace std;
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/normal_distribution.hpp>
+#include <boost/random/taus88.hpp>
+
+#include "rngbenchmark.h"
+
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    const int SEED = 235;
+    const unsigned long TRIALS = 1 << 27;
+
+    std::vector<Benchmark::Benchmark*> benchmarks = {
+        new Benchmark::RNGBenchmark<std::mt19937, std::normal_distribution<float>, float>(
+            "std::mt199937 and std::normal_distribution in float precision",
+            std::normal_distribution<float>(0, 1),
+            SEED, TRIALS
+        ),
+        new Benchmark::RNGBenchmark<std::mt19937, std::normal_distribution<double>, double>(
+            "std::mt199937 and std::normal_distribution in double precision",
+            std::normal_distribution<double>(0, 1),
+            SEED, TRIALS
+        ),
+        new Benchmark::RNGBenchmark<std::mt19937_64, std::normal_distribution<float>, float>(
+            "std::mt199937_64 and std::normal_distribution in float precision",
+            std::normal_distribution<float>(0, 1),
+            SEED, TRIALS
+        ),
+        new Benchmark::RNGBenchmark<std::mt19937_64, std::normal_distribution<double>, double>(
+            "std::mt199937_64 and std::normal_distribution in double precision",
+            std::normal_distribution<double>(0, 1),
+            SEED, TRIALS
+        ),
+        new Benchmark::RNGBenchmark<std::minstd_rand, std::normal_distribution<float>, float>(
+            "std::minstd_rand and std::normal_distribution in float precision",
+            std::normal_distribution<float>(0, 1),
+            SEED, TRIALS
+        ),
+        new Benchmark::RNGBenchmark<std::minstd_rand, std::normal_distribution<double>, double>(
+            "std::minstd_rand and std::normal_distribution in double precision",
+            std::normal_distribution<double>(0, 1),
+            SEED, TRIALS
+        ),
+        new Benchmark::RNGBenchmark<boost::random::mt19937,
+                                    boost::random::normal_distribution<float>, float>(
+            "boost::random::mt19937 and boost::random::normal_distribution in float precision",
+            boost::random::normal_distribution<float>(0, 1),
+            SEED, TRIALS
+        ),
+        new Benchmark::RNGBenchmark<boost::random::mt19937,
+                                    boost::random::normal_distribution<double>, double>(
+            "boost::random::mt19937 and boost::random::normal_distribution in double precision",
+            boost::random::normal_distribution<double>(0, 1),
+            SEED, TRIALS
+        ),
+        new Benchmark::RNGBenchmark<boost::random::mt19937_64,
+                                    boost::random::normal_distribution<float>, float>(
+            "boost::random::mt19937_64 and boost::random::normal_distribution in float precision",
+            boost::random::normal_distribution<float>(0, 1),
+            SEED, TRIALS
+        ),
+        new Benchmark::RNGBenchmark<boost::random::mt19937_64,
+                                    boost::random::normal_distribution<double>, double>(
+            "boost::random::mt19937_64 and boost::random::normal_distribution in double precision",
+            boost::random::normal_distribution<double>(0, 1),
+            SEED, TRIALS
+        ),
+        new Benchmark::RNGBenchmark<boost::random::mt11213b,
+                                    boost::random::normal_distribution<float>, float>(
+            "boost::random::mt11213b and boost::random::normal_distribution in float precision",
+            boost::random::normal_distribution<float>(0, 1),
+            SEED, TRIALS
+        ),
+        new Benchmark::RNGBenchmark<boost::random::mt11213b,
+                                    boost::random::normal_distribution<double>, double>(
+            "boost::random::mt11213b and boost::random::normal_distribution in double precision",
+            boost::random::normal_distribution<double>(0, 1),
+            SEED, TRIALS
+        ),
+        new Benchmark::RNGBenchmark<boost::random::taus88,
+                                    boost::random::normal_distribution<float>, float>(
+            "boost::random::taus88 and boost::random::normal_distribution in float precision",
+            boost::random::normal_distribution<float>(0, 1),
+            SEED, TRIALS
+        ),
+        new Benchmark::RNGBenchmark<boost::random::taus88,
+                                    boost::random::normal_distribution<double>, double>(
+            "boost::random::taus88 and boost::random::normal_distribution in double precision",
+            boost::random::normal_distribution<double>(0, 1),
+            SEED, TRIALS
+        ),
+    };
+
+    for (const auto &bm : benchmarks) {
+        bm->run();
+    }
+
     return 0;
 }
